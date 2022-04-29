7fb2a225baaccb7e613ff5fdf8c6f992c2a993ec
Vibhum Raj Tripathi <vibhumrajt.developer@gmail.com>
2022-04-30 00:59:31 +0530

    view only students

--- frontend/src/components/List Items/student_item.js
+++ frontend/src/components/List Items/student_item.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import { useContext } from "react";
 import { useNavigate } from "react-router";
+import { UserContext } from "../contexts/UserContext";
 import "./student_item.css"
 
 
 const StudentItem = (props) => {
     const navigate = useNavigate();
-
+    const {user} = useContext(UserContext);
     
     const delStudent = function(){
         axios.post("http://localhost:3010/student/delete",{student_id:props.user.student_id}).then(()=>navigate(0));
@@ -17,7 +19,7 @@ const StudentItem = (props) => {
             <h4>{props.user.first_name}</h4>
             <h4>{props.user.last_name}</h4>
             <button className = 'view button btn btn-primary' onClick={_ => navigate("/student/view/" + props.user.student_id)}>View</button>
-            <button className = 'delete button btn btn-primary' onClick={delStudent}>Delete</button>
+            {user.role==="admin" && <button className = 'delete button btn btn-primary' onClick={delStudent}>Delete</button>}
             <br></br>
         </span>
      );
